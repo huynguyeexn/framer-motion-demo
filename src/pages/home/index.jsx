b92779a2a1305a1922549d7a11df4a4b6b61c1b2
huynguyeexn <huynguyeexn@gmail.com>
2024-05-05 10:13:44 +0700

    Demo 1: Animating Elements

--- src/pages/home/index.jsx
+++ src/pages/home/index.jsx
@@ -1,6 +1,7 @@
 import React, { useContext } from "react";
 import { Link } from "react-router-dom";
 import { routersObj } from "../../common/routes";
+import { motion } from "framer-motion";
 
 import "./style.css";
 import { PizzaContext } from "../../App";
@@ -9,9 +10,24 @@ const HomePage = () => {
   const { resetPizza } = useContext(PizzaContext);
   return (
     <div className="home container">
-      <h2>Welcome to Pizza Joint</h2>
+      <motion.h2
+        animate={{
+          fontSize: "50px",
+          color: "#ff2994",
+          y: -50,
+        }}
+      >
+        Welcome to Pizza Joint
+      </motion.h2>
       <Link to={routersObj.base.path} onClick={resetPizza}>
-        <button>Create Your Pizza</button>
+        <motion.button
+          animate={{
+            rotateZ: "360deg",
+            scale: 1.5,
+          }}
+        >
+          Create Your Pizza
+        </motion.button>
       </Link>
     </div>
   );
